4726faadfbd19caf2bb2325f1e28d3bcf6550f6e
leonardoserra <serra.leonardo92@gmail.com>
2024-04-29 10:16:13 +0200

    added linktree

--- app/components/LogoHero.tsx
+++ app/components/LogoHero.tsx
@@ -4,29 +4,30 @@ import Logo from '@/public/big_logo_hero.png'
 
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+
 gsap.registerPlugin(useGSAP);
 
 export default function LogoHero() {
   
   useGSAP(() => {
-   
     gsap.to(".logo-container", {y: -80,  ease: "bounce.in", duration:3});
-
   });
 
 
   return (
-    <section className="mt-32 mb-40 m-auto">
+    <section id="logo-section" className="mt-32 mb-40 m-auto">
       <div className="m-auto w-[700px] h-[700px] logo-container">
-        <Image
-          
-          alt="Logo Hero"
-          src={Logo}
-          width={700}
-          height={700}
-          quality={100}
-          priority={true}
-        />
+        <a href="https://linktr.ee/Daruma" target="_blank" rel="noopener noreferrer">
+          <Image
+            id="logo-image"
+            alt="Logo Hero"
+            src={Logo}
+            width={700}
+            height={700}
+            quality={100}
+            priority={true}
+          />
+        </a>
       </div>
     </section>
 
