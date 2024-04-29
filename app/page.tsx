7ad06f6f7b7222f595d459474758a28e776c06a8
leonardoserra <serra.leonardo92@gmail.com>
2024-04-30 01:02:55 +0200

    removed the homepage route, useless

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import HomePage from "./homepage/page"; 
+import dynamic from 'next/dynamic'
+ 
+// Client Components:
+const LogoHero = dynamic(() => import('./components/LogoHero'))
+const ContentImageSection = dynamic(() => import('./components/ContentImageSection'))
+const ImageContentSection = dynamic(() => import('./components/ImageContentSection'))
+const ImagesList = dynamic(() => import('./components/ImagesList'))
+// const SpotifyAlbumsList = dynamic(() => import('./components/SpotifyAlbumsList'))
 
+// import LogoHero from "./components/LogoHero";
+// import ContentImageSection from "./components/ContentImageSection";
+// import ImageContentSection from "./components/ImageContentSection";
+// import ImagesList from "./components/ImagesList";
+
+import SpotifyAlbumsList from "./components/SpotifyAlbumsList";
 export default function Home() {
   return (
     <main>
@@ -8,7 +21,11 @@ export default function Home() {
         <div className="cursor cursor--large"></div>
         <div className="cursor cursor--small"></div>
       </div>
-        <HomePage/>
+        <LogoHero/>
+        <ContentImageSection/>  
+        <ImagesList/>{/* Still not a carousel */}
+        <ImageContentSection/> 
+        <SpotifyAlbumsList />
       </div>
     </main>   
   );
