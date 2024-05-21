f86ab39ce063bbcdd3fce2bf613d76f1bba1203a
leonardoserra <serra.leonardo92@gmail.com>
2024-05-21 15:57:30 +0200

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,15 +6,33 @@ import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AudioPlayer from "./components/AudioPlayer";
+import LogoHero from "./components/LogoHero";
+import ContentImageSection from "./components/ContentImageSection";
+import ImageContentSection from "./components/ImageContentSection";
+import ImagesList from "./components/ImagesList";
+import SpotifyAlbumsList from "./components/SpotifyAlbumsList";
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
+
 const ruluko = Ruluko({subsets: ["latin"], weight: "400", display:"swap", preload: true});
-//to use the font class  className={rubik_bubbles.className}
 
 export const metadata: Metadata = {
   title: "Daruma.io",
   description: "Daruma, beatmaker and music producer from Italy",
+  generator: 'Next.js',
+  applicationName: 'Daruma | Beatmaker',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript', 'Daruma', 'Beatmaker', 'Music'],
+  authors: [{ name: 'Leonardo Serra' }],
+  creator: 'Leonardo Serra',
+  publisher: 'github',
+  formatDetection: {
+    email: true,
+    address: false,
+    telephone: false,
+  },
 };
+
 
 export default function RootLayout({
   children,
@@ -26,7 +44,12 @@ export default function RootLayout({
       
       <body className={`${ruluko.className} text-primaryBlack relative`}>
         <Header/>
-        {children}
+        <LogoHero/>
+        <ContentImageSection/>  
+        <ImagesList/>
+        <ImageContentSection/> 
+        {/* {children} */}
+        <SpotifyAlbumsList/>
         <Footer/>
         <AudioPlayer/>
       </body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,47 +1,11 @@
-import dynamic from 'next/dynamic'
- 
-// Client Components:
-// const LogoHero = dynamic(() => import('./components/LogoHero'))
-// const ContentImageSection = dynamic(() => import('./components/ContentImageSection'))
-// const ImageContentSection = dynamic(() => import('./components/ImageContentSection'))
-// const ImagesList = dynamic(() => import('./components/ImagesList'))
-// const SpotifyAlbumsList = dynamic(() => import('./components/SpotifyAlbumsList'))
-
-import LogoHero from "./components/LogoHero";
-import ContentImageSection from "./components/ContentImageSection";
-import ImageContentSection from "./components/ImageContentSection";
-import ImagesList from "./components/ImagesList";
-
-import SpotifyAlbumsList from "./components/SpotifyAlbumsList";
-
-export const metadata = {
-  generator: 'Next.js',
-  applicationName: 'Daruma | Beatmaker',
-  referrer: 'origin-when-cross-origin',
-  keywords: ['Next.js', 'React', 'JavaScript', 'Daruma', 'Beatmaker', 'Music'],
-  authors: [{ name: 'Leonardo Serra' }],
-  creator: 'Leonardo Serra',
-  publisher: 'github',
-  formatDetection: {
-    email: true,
-    address: false,
-    telephone: false,
-  },
-}
-
 export default function Home() {
   return (
     <main>
       <div className="mt-12 text-center">
-      <div className="custom-cursor">
-        <div className="cursor cursor--large"></div>
-        <div className="cursor cursor--small"></div>
-      </div>
-        <LogoHero/>
-        <ContentImageSection/>  
-        <ImagesList/>{/* Still not a carousel */}
-        <ImageContentSection/> 
-        <SpotifyAlbumsList/>
+        <div className="custom-cursor">
+          <div className="cursor cursor--large"></div>
+          <div className="cursor cursor--small"></div>
+        </div>
       </div>
     </main>   
   );
